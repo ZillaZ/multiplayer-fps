b829f8fd97af171415a1616304bfbad594ba1e4a
Lucas <lucasrustacean@gmail.com>
2024-04-04 22:14:05 -0300

    client stopping server when quitting fixed

--- src/main.rs
+++ src/main.rs
@@ -100,24 +100,23 @@ async fn handle_connection(
     let mut buf = [0; 1024];
     let mut manager = None;
     let _ = manager;
-    while let Ok(data) = stream.read(&mut buf).await {
+    while let Ok(_data) = stream.read(&mut buf).await {
         manager = Some(rec_clone.recv().unwrap());
         if !control {
             player = Some(new_player(&mut manager.as_mut().unwrap()));
             control = true;
         }
-        if data > 0 {
-            let state = PlayerSignal::from_bytes((&buf, 0)).unwrap().1;
-            let signal = player.as_mut().unwrap().update(
-                &mut manager.as_mut().unwrap(),
-                state.clone(),
-                state.dt,
-                Vector3::new(0.0, -9.81, 0.0),
-            );
-            manager.as_mut().unwrap().dt = state.dt;
-            channel.send(manager.as_ref().unwrap().clone()).unwrap();
-            let _count = stream.write(&signal.to_bytes().unwrap()).await.unwrap();
-        }
+        let state = PlayerSignal::from_bytes((&buf, 0)).unwrap().1;
+        let signal = player.as_mut().unwrap().update(
+            &mut manager.as_mut().unwrap(),
+            state.clone(),
+            state.dt,
+            Vector3::new(0.0, -9.81, 0.0),
+        );
+        manager.as_mut().unwrap().dt = state.dt;
+        channel.send(manager.as_ref().unwrap().clone()).unwrap();
+        let _count = stream.write(&signal.to_bytes().unwrap()).await.unwrap();
+        stream.flush().await.unwrap();
     }
     manager = Some(rec_clone.recv().unwrap());
     manager.as_mut().unwrap().remove_player(&player.unwrap().id);
